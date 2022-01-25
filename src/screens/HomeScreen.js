28f8724d3a5c84319dfd2dc321c39279cd14d3a8
swalihmoosa <swalihmoosa72@gmail.com>
2022-01-25 16:13:35 +0530

    HomeScreen | ongoing

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,17 @@
-import react, {useState} from 'react';
+import react, {useState, useEffect} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image, FlatList, Dimensions } from 'react-native';
 
 var logo = require ('../../assets/images/Comics-Iron-America-icon.png');
 
+const {width, height} = Dimensions.get("screen")
+const setWidth = (w) => (width / 100) * w
+
 
 export default function HomeScreen() {
-    const [characters, setCharacters] = useState([]);
+    const [characters, setCharacters] = useState(['a', 'b', 'c', 'd', 'e']);
+
+    
 
     return (
         <ScrollView >
@@ -16,6 +21,11 @@ export default function HomeScreen() {
 			    <Image source={logo} style={styles.logoimage} />
 			    <Text style={styles.loginbutton}>Logout</Text>
 		    </View>
+
+            <FlatList data={characters} horizontal keyExtractor={(item)=> item} renderItem={({item, index})=>{
+                return <View style={styles.test} ></View>
+            }} ></FlatList>
+
         </ScrollView>
     );
 }
@@ -44,5 +54,17 @@ const styles = StyleSheet.create({
 		fontWeight:'900',
 		alignItems:'center',
 		justifyContent:'center',
+	},
+    test: {
+		backgroundColor: '#046bf6',
+		borderRadius:4,
+		paddingVertical:13,
+		paddingHorizontal:45,
+		color:'#fff',
+		fontSize:18,
+		fontWeight:'900',
+		alignItems:'center',
+		justifyContent:'center',
+        width : setWidth(23),
 	},
 });
