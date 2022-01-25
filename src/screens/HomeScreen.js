db334d435a8c03c83edbff9cd87968471fc16d2b
swalihmoosa <swalihmoosa72@gmail.com>
2022-01-25 15:10:25 +0530

    Navigator | setup

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,20 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
+
+var logo = require ('../../assets/images/Comics-Iron-America-icon.png');
+
 
 export default function HomeScreen() {
-  return (
-    <View style={styles.container}>
-      <Text>Home screen!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <ScrollView >
+            <StatusBar style="auto" />
+
+            <View style={styles.headercontainer}>
+			    <Image source={logo} style={styles.logoimage} />
+			    <Text style={styles.loginbutton}>Logout</Text>
+		    </View>
+        </ScrollView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    headercontainer : {
+        width:'90%',
+        marginLeft:'auto',
+        marginRight:'auto',
+        color:'red',
+        padding:'10%',
+        justifyContent:'space-between',
+		flexDirection:'row',
+    },
+	logoimage: {
+		width: 65,
+		height:65,
+	},
+	loginbutton: {
+		backgroundColor: '#046bf6',
+		borderRadius:4,
+		paddingVertical:13,
+		paddingHorizontal:45,
+		color:'#fff',
+		fontSize:18,
+		fontWeight:'900',
+		alignItems:'center',
+		justifyContent:'center',
+	},
 });
